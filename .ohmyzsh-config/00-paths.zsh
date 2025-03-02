f38932bed79c82279a81b63988745190b4cd8e33
Jay Burgess <jay@rogueserenity.com>
2025-03-01 20:10:07 -0700

    make a couple of tweaks

--- .ohmyzsh-config/00-paths.zsh
+++ .ohmyzsh-config/00-paths.zsh
@@ -1,1 +1,2 @@
-export PATH=/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin
+export PATH=~/.local/bin:/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin
+

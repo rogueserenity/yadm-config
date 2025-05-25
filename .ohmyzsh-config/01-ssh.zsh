e451adb4e548196450628f78b1bb812205994e8d
Jay Burgess <jay@rogueserenity.com>
2025-05-25 17:32:59 -0600

    update ssh alias

--- .ohmyzsh-config/01-ssh.zsh
+++ .ohmyzsh-config/01-ssh.zsh
@@ -1,4 +1,4 @@
 # Secretive Config
 export SSH_AUTH_SOCK=/Users/jay/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
 
-alias dns-pi="ssh pi@192.168.1.11"
+alias beret="ssh jay@192.168.1.11"

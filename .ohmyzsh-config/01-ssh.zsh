6c013014c277d72048302edb01a0f444e674cc19
Jay Burgess <jay@rogueserenity.com>
2026-01-10 17:31:28 -0700

    fixes

--- .ohmyzsh-config/01-ssh.zsh
+++ .ohmyzsh-config/01-ssh.zsh
@@ -1,1 +1,1 @@
-export SSH_AUTH_SOCK=/Users/jay/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock
+export SSH_AUTH_SOCK='/Users/jay/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock'
